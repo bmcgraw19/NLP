49d0f95766327ab29d407a5a8e434223bfcbac48
Brannon McGraw <bmcgraw@gwmail.gwu.edu>
2013-03-04 19:53:00 -0500

    finished bigram

--- letterLangId.pl
+++ letterLangId.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/perl
-use warnings;
-use strict; 
+use Switch; 
 
 require 'common_functions.pl';
 
@@ -101,30 +100,32 @@ foreach $line (@lines){ #each line
 #does the same, but for the french corpus
 foreach $line (<FR>){
     $line = depunct($line);
-    my (@words) = split /\s+/, $line;
-    foreach my $word (@words){
-	my $len = 0;
-
-	$letter_fre{ (substr $word, 0, 1)}++;
-	$start_fre{(substr $word, 0, 1)}++;
-	if((substr $word, 0,1) ne ""){
-	    $num_le_fre ++;
-	    $sn_fre ++;
-	}
-	while($len < (length($word) - 1)){
-	    $letter_fre{ (substr $word, $len+1, 1)}++;
-	    if((substr $word, $len+1, 1) ne ""){
-		$num_le_fre ++;
-	    }
+    if($line){
+	my (@words) = split /\s+/, $line;
+	foreach my $word (@words){
+	    my $len = 0;
 	    
+	    $letter_fre{ (substr $word, 0, 1)}++;
+	    $start_fre{(substr $word, 0, 1)}++;
+	    if((substr $word, 0,1) ne ""){
+		$num_le_fre ++;
+		$sn_fre ++;
+	    }
+	    while($len < (length($word) - 1)){
+		$letter_fre{ (substr $word, $len+1, 1)}++;
+		if((substr $word, $len+1, 1) ne ""){
+		    $num_le_fre ++;
+		}
+		
 	    my $fre_gram = substr $word, $len, 2;
-	    $bigram_fre{$fre_gram}++;
-	    $num_bi_fre ++;
-
-	    $len++;
+		$bigram_fre{$fre_gram}++;
+		$num_bi_fre ++;
+		
+		$len++;
+	    }
+	    $end_fre{substr $word, -1, 1}++;
+	    $en_fre ++;
 	}
-	$end_fre{substr $word, -1, 1}++;
-	$en_fre ++;
     }
 }
 
@@ -165,51 +166,140 @@ print OUT "ID LANG\n";
 @lines = (<IN>);
 while ( my ($index, $line) = each @lines){ 
     print OUT $index+1 . ". ";   #prints the line number
-    chomp($line);    
-    my $lang = 0;
+    chomp($line);   
+    $line = depunct($line);
+    if($line =~ m/\w/){
+	my $lang = 0;
     
-    #splits into words
-    my (@words) = split /\s+/, $line;
-    foreach my $word (@words){
-	#determines what language the word is most likely to be
-	my $eng_prob = 0;
+	my $eng_prob = 0; #total probability of the sentence
 	my $fre_prob = 0;
 	my $ger_prob = 0;
-
-	my $len = 0;
-	#calculates probablility of the first letter p(c_0)
-	while($len < (length($word) - 1)){ 
-	    my $ngm = substr $word, $len, 2;  #splits word into bigrams
+	
+	my $ew_prob = 0; #probability of the word
+	my $fw_prob = 0;
+	my $gw_prob = 0;
+	
+	#splits into words
+	my (@words) = split /\s+/, $line;
+	foreach my $word (@words){
 	    
-	    #ENGLISH PROBABILITY
-	    $ew_prob = ($bigram_eng{$ngm} /$num_bi_eng) ;     # P(n | n-1) = P( n-1 n)
-	    $ew_prob /= ($letter_eng{substr $word, $len+1, 1} / $num_le_eng ); #/ P( n-1)
-	    $eng_prob += $ew_prob;
-
-	    #FRENCH PROBABILITY
-	    $fw_prob = ($bigram_fre{$ngm} / $num_bi_fre);
-	    $fw_prob /= ($letter_fre{substr $word, $len+1, 1} / $num_le_fre);
-	    $fre_prob += $fw_prob;
+	    #determines what language the word is most likely to be: 
+	    #calculates probablility of the first letter p(c_0)
 	    
-	    #GERMAN PROBABLILITY
-	    $gw_prob = ($bigram_ger{$ngm}/$num_bi_ger);
-	    $gw_prob /= ($letter_ger{substr $word, $len+1, 1} / $num_le_ger);
-	    $ger_prob += $gw_prob;
-	    $len++;
-	}
-    }
-	#Computes which is the largest
-	if( $eng_prob >= $fre_prob){
-	    if($eng_prob >= $ger_prob){
-		$lang = 1;
+	    #add-one smoothing to prevent division by zero
+	    if($start_eng{substr $word, 0, 1} == 0){
+		$start_eng{substr $word, 0, 1} ++;
+		$sn_eng ++;
 	    }
+	    if($start_fre{substr $word, 0, 1} == 0){
+		$start_fre{substr $word, 0, 1} ++;
+		$sn_fre ++;
+	    }
+	    if($start_ger{substr $word, 0, 1} == 0){
+		$start_ger{substr $word, 0, 1} ++;
+		$sn_ger ++;
+	    }
+	    
+	    $ew_prob = ($start_eng{substr $word, 0, 1} / $sn_eng)*1000;
+	    $fw_prob = ($start_fre{substr $word, 0, 1} / $sn_fre)*1000;
+	    $gw_prob = ($start_ger{substr $word, 0, 1} / $sn_ger)*1000;
+	    
+	    my $len = 0;
+	    while($len < (length($word) - 1)){ 
+		my $ngm = substr $word, $len, 2;  #splits word into bigrams
+		
+		
+		#add-one smoothing to prevent divison by zero
+		if($bigram_eng{$ngm} == 0){
+		    $bigram_eng{$ngm} ++;
+		    $num_bi_eng ++;
+		}
+		if($bigram_fre{$ngm} == 0){
+		    $bigram_fre{$ngm}++;
+		    $num_bi_fre ++;
+		}
+		if($bigram_ger{$ngm} == 0){
+		    $bigram_ger{$ngm}++;
+		    $num_bi_ger ++;
+		}
+		if($letter_eng{substr $word, $len, 1} == 0){
+		    $letter_eng{substr $word, $len, 1} ++;
+		    $num_le_eng ++;
+		}
+		if($letter_fre{substr $word, $len, 1} == 0){
+		    $letter_fre{substr $word, $len, 1} ++;
+		    $num_le_fre ++;
+		}
+		if($letter_ger{substr $word, $len, 1} == 0){
+		    $letter_ger{substr $word, $len, 1} ++;
+		    $num_le_ger ++;
+		}
+		
+		
+		#ENGLISH PROBABILITY
+		$ew_prob *= 1000;
+		$ew_prob *= ($bigram_eng{$ngm} / $num_bi_eng) ;     # P(n | n-1) = P( n-1 n)
+		$ew_prob /= ($letter_eng{substr $word, $len, 1} / $num_le_eng ); #/ P( n-1)
+		
+		
+		#FRENCH PROBABILITY
+		$fw_prob *= 1000;
+		$fw_prob *= ($bigram_fre{$ngm} / $num_bi_fre);
+		$fw_prob /= ($letter_fre{substr $word, $len, 1} / $num_le_fre);
+		
+		
+		#GERMAN PROBABLILITY
+		$gw_prob *= 1000;
+		$gw_prob *= ($bigram_ger{$ngm}/$num_bi_ger);
+		$gw_prob /= ($letter_ger{substr $word, $len, 1} / $num_le_ger);
+		
+		$len++;
+	    }#end while (done word)
+	    
+	    #add-one smoothing to prevent divison by zero
+	    if($end_eng{substr $word, -1, 1} == 0){
+		$end_eng{substr $word, -1, 1} ++;
+		$en_eng ++;
+	    }
+	    if($end_fre{substr $word, -1, 1} == 0){
+		$end_fre{substr $word, -1, 1} ++;
+		$en_fre ++;
+	    }
+	    if($end_ger{substr $word, -1, 1} == 0){
+		$end_ger{substr $word, -1, 1} ++;
+		$en_ger ++;
+	    }
+	    
+	    
+	    
+	    #calculates probability of the last letter p(c_n)
+	    $ew_prob *= ($end_eng{substr $word, -1, 1} / $en_eng );
+	    $fw_prob *= ($end_fre{substr $word, -1, 1} / $en_fre );
+	    $gw_prob *= ($end_ger{substr $word, -1, 1} / $en_ger );
+	    
+	    #now, *_prob is equal to the entire probability of that word
+	    $eng_prob += $ew_prob; #so we sum these probabilities
+	    $fre_prob += $fw_prob;
+	    $ger_prob += $gw_prob;
 	}
+	#Computes which is the largest
+	if($eng_prob > $fre_prob && $eng_prob > $ger_prob){
+	    $lang = 1;
+	}
+	if($fre_prob > $eng_prob && $fre_prob > $ger_prob){
+	    $lang = 2;
+	}
+	if($ger_prob > $eng_prob && $ger_prob > $fre_prob){
+	    $lang = 3;
+	}
+	
+	switch ($lang){
+	    case 0 {print OUT "ERRRRRRRRR\n"}
+	    case 1 {print OUT "EN\n"}
+	    case 2 {print OUT "FR\n"}
+	    case 3 {print OUT "GR\n"}
+	} #end while (done parsing in file) 
     }
-    switch ($lang){
-	case 0 {print OUT "ERR\n"}
-	case 1 {print OUT "ENG\n"}
-	case 2 {print OUT "FRE\n"}
-	case 3 {print OUT "GER\n"}
 }
 
 close(EN);
